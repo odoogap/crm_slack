7d788184186175cf11dc3af7665d46a8915393d2
Diogo Duarte <dduarte@odoogap.com>
2018-03-22 09:51:25 +0000

    [IMP] added documentation

--- crm_slack/__manifest__.py
+++ crm_slack/__manifest__.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Slack CRM integration',
-    'version': '1.0',
+    'version': '10.0',
     'author': 'OdooGAP',
     'summary': 'Slack CRM integration',
     'description': """
 Slack CRM integration
 =====================
-Get Notification when u have a new lead
+Get Notification when you have a new lead
+
+# Odoo Module For Slack
+
+Odoo Module for Slack. You get a warning on a specific channel, every time a new lead is created.
+
+## Configuration
+
+In Sales >> Settings find (Slack integration) or
+
+ir.config_parameter
+
+In Settings > Technical > Parameters > System Parameters
+
+slack.sales_channel --> Channel where want to get the crm leads
+slack.api_token --> the slack api token
 
     """,
     'website': 'http://www.odoogap.com',
